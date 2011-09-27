45fdc6e6874f5f000813906e15267b24099f3347
Robert Bocchino <rbocchin@cs.cmu.edu>
2011-09-26 20:23:35 -0400

    Add placeholder test

---
+++ Implementation/Compiler/test/dpj-junit-tests/Placeholder.java
@@ -0,0 +1,20 @@
+/**
+ * Some placeholder tests
+ * 
+ * @author Rob Bocchino
+ */
+
+import org.junit.Test;
+
+
+public class Placeholder extends DPJTestCase {
+    
+    public Placeholder() {
+	super("Placeholder");
+    }
+    
+    @Test public void testPlaceholder() throws Throwable {
+	compile("Placeholder");
+    }
+    
+}
