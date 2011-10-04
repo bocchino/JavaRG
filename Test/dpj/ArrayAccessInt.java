f715253b488ca4d4ea0da1b578e48c5d4350fff3
Robert Bocchino <rbocchin@cs.cmu.edu>
2011-10-04 14:17:54 -0400

    Add Test directory and harness.  Add ArrayAccessInt test.

---
+++ Test/dpj/ArrayAccessInt.java
@@ -0,0 +1,37 @@
+/**
+* Test array class access with 'int' cell type
+*/
+
+arrayclass IntArray { int; }
+
+class ArrayAccessInt extends Harness {
+    IntArray array;
+
+    @Override
+    public void initialize() {
+	array = new IntArray(size);
+    }
+
+    @Override
+    public void runTest() {
+	for (int i = 0; i < size; ++i) {
+	    assert (array[i] == i);
+	}
+    }
+
+    @Override
+    public void runWork() {
+	for (int i = 0; i < size; ++i) {
+	    array[i] = i;
+	}
+    }
+
+    public ArrayAccessInt(String[] args) {
+	super("ListRanking", args);
+    }
+
+    public static void main(String[] args) {
+	ArrayAccessInt test = new ArrayAccessInt(args);
+	test.run();
+    }
+}
